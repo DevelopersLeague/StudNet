5d336f6d8acfa8d95ce1828192c7e14048e19e0e
aniket more <aniketavinashmore343@gamil.com>
2020-10-23 19:54:46 +0530

    add models
    add question, answer, category, update models

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+# question class
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+
+class Question(models.Model):
+    user_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    question_text = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+
+class Answer(models.Model):
+    user_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+
+class Update(models.Model):
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    update_text = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
