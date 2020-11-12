0af18e653320aef21c538b28d432a66adddfff0c
aniket <aniketavinashmore343@gmail.com>
2020-11-12 18:49:39 +0530

    added questions to the homepage and question_display page

--- forum/urls.py
+++ forum/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path("register/", views.registerPage, name="register"),
     path("login/", views.loginPage, name="login"),
     path("logout/", views.logoutUser, name="logout"),
+    path('', views.landingPage, name='landingPage'),
     path('home/', views.home, name='home'),
-    path('', views.landingPage, name='landingPage')
+    path('forum/<int:page>/', views.forum, name='forum'),
+    path('question/<int:id>/', views.question_display, name='question_display')
+
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate, login, logout 
-from django.contrib import messages # for flash messages 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages  # for flash messages
 from .models import *
 from .forms import CreateUserForm
-from django.contrib.auth.decorators import login_required   # for restricting users without login 
+# for restricting users without login
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
 
 def registerPage(request):
     if request.user.is_authenticated:
@@ -19,17 +21,20 @@ def registerPage(request):
             if form.is_valid():
                 form.save()
                 user_msg = form.cleaned_data.get('username')
-                messages.success(request, 'Account was created for '+user_msg )
+                messages.success(request, 'Account was created for '+user_msg)
 
                 # redirecting user after successful login
-                return redirect('login')   # you can also place landingPage here  
+                # you can also place landingPage here
+                return redirect('login')
 
-        context = {'form':form}
+        context = {'form': form}
         return render(request, 'forum/register.html', context)
+
 
 def loginPage(request):
     if request.user.is_authenticated:
         return redirect('home')
+        # return redirect('forum/1')
     else:
         if request.method == 'POST':
             username = request.POST.get('username')
@@ -40,12 +45,15 @@ def loginPage(request):
 
             if user is not None:
                 login(request, user)
-                return redirect('home')    # landingpage should come here DOUBTFULL, earlier it was home
+                # landingpage should come here DOUBTFULL, earlier it was home
+                return redirect('home')
+                # return redirect('forum/1')
             else:
                 messages.info(request, 'Username OR Password is incorrect')
 
         context = {}
         return render(request, 'forum/login.html', context)
+
 
 def logoutUser(request):
     logout(request)
@@ -54,12 +62,31 @@ def logoutUser(request):
 # @login_required(login_url='landing')
 # put the below line above every view that you want to restrict if the user hasn't loggedin
 # these are nothing but login decorators
+
+
 @login_required(login_url='login')
 def home(request):
     # context = {}
-	return render(request, 'forum/main.html', {})
+    # return render(request, 'forum/main.html', {})
+    return redirect("forum", page=1)
+
+
+@login_required(login_url='login')
+def forum(request, page):
+    questions = Question.objects.all().order_by('-created_on')
+    min_index = (page-1)*10
+    max_index = min((page*10)-1, len(questions)-1)
+    context = {'questions': questions[min_index:max_index+1]}
+    return render(request, 'forum/forum.html', context)
+
+
+@login_required(login_url='login')
+def question_display(request, id):
+    question = Question.objects.get(id=id)
+    answers = question.answer_set.all()
+    context = {'question': question, 'answers': answers}
+    return render(request, 'forum/question_display.html', context)
 
 
 def landingPage(request):
     return render(request, 'forum/landingPage.html', {})
-
