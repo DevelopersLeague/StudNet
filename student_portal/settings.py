1de533c015afd188d64dc0b72b5bd4b133f6e5e4
aniket more <aniketavinashmore343@gmail.com>
2021-03-25 16:42:10 +0530

    add dj_database

--- student_portal/settings.py
+++ student_portal/settings.py
@@ -180,6 +180,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env);
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
