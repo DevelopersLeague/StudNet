fc76ad407355a7078cb52a3ffdc0ad9d88819d6e
aniket more <aniketavinashmore343@gmail.com>
2021-03-25 16:23:25 +0530

    add static files

--- student_portal/settings.py
+++ student_portal/settings.py
@@ -182,6 +182,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-#  STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join('static'), )
